158f7657c677c86b7a4ea4d2be91d58ab1747e01
CakeMonster987 <mikkelboegh15@gmail.com>
2023-03-14 10:28:29 +0100

    Enemy movement
    Har lavet basic movement til en goblin og en wizard

---
+++ Assets/Scripts/WizardMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardMove : MonoBehaviour
+{
+    public int Speed;
+    private float RunSpeed;
+    public int hp;
+
+    Rigidbody2D rigidbody;
+
+    public Transform Player;
+    public float detectionrange;
+    public float runrange;
+    Transform currentTarget;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        currentTarget = Player;
+        runrange = 6;
+
+        hp = 5;
+        Speed = 4;
+        RunSpeed = -0.3f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = currentTarget.position - transform.position;
+        direction.Normalize();
+        rigidbody.velocity = direction * Speed;
+
+        if (runrange > Vector3.Distance(transform.position, Player.position))
+        {
+            rigidbody.velocity = direction / RunSpeed;
+        }
+
+
+    }
+}
