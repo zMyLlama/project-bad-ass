b135148ecf6663d80811669aaa8f1a393c88d5ac
zMyLlama <noelgamsboel@gmail.com>
2023-03-28 09:25:01 +0200

    changed velocity settings

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -22,14 +22,13 @@ public class Movement : MonoBehaviour
     #region Private Variables
     [Header("Serialized")]
     [SerializeField] Rigidbody2D _rb;
+    Dictionary<int, string> representingAnimationNames = new Dictionary<int, string>();
     #endregion
 
     #region Methods
     private IEnumerator walkLoop() {
         while (true)
         {
-            playerAnimator.Play("Walk");
-
             GameObject _dustCloudClone = Instantiate(dustCloud, feetLocation.position, Quaternion.identity);
             _dustCloudClone.transform.rotation = Quaternion.Euler(0, 0, Random.Range(-180f, 180f));
             _dustCloudClone.transform.localScale = new Vector3(Random.Range(4f, 6f), Random.Range(4, 6f), 1);
@@ -43,6 +42,11 @@ public class Movement : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        representingAnimationNames.Add(0, "WalkUp");
+        representingAnimationNames.Add(1, "WalkSide");
+        representingAnimationNames.Add(2, "WalkDown");
+        representingAnimationNames.Add(3, "WalkSide");
     }
     #endregion
 
@@ -50,6 +54,7 @@ public class Movement : MonoBehaviour
     KeyCode _lastPressedXVelocityKey;
     KeyCode _lastPressedYVelocityKey;
     IEnumerator _walkLoopCoroutine = null;
+    int _currentPlayingAnimationIndex;
     void Update()
     {
         foreach (var key in movementKeybinds) {
@@ -57,6 +62,9 @@ public class Movement : MonoBehaviour
 
             if (Input.GetKeyDown(key))
             {
+                playerAnimator.Play(representingAnimationNames[_keybindLocationInList]);
+                _currentPlayingAnimationIndex = _keybindLocationInList;
+
                 if (_walkLoopCoroutine == null) {
                     _walkLoopCoroutine = walkLoop();
                     StartCoroutine(_walkLoopCoroutine);
@@ -87,6 +95,9 @@ public class Movement : MonoBehaviour
         }
 
         _rb.velocity = new Vector2(xVelocity, yVelocity);
+        if (xVelocity < 0) { transform.localScale = new Vector3(-1, 1, 1); } else if (xVelocity > 0) { transform.localScale = new Vector3(1, 1, 1); };
+        playerAnimator.SetFloat("velocityX", xVelocity);
+        playerAnimator.SetFloat("velocityY", yVelocity);
     }
     #endregion
 }
