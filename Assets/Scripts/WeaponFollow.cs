dd477bb702e6f46832ad2f87a3021876727ebf6d
zMyLlama <noelgamsboel@gmail.com>
2023-05-02 08:50:55 +0200

    fixed weapon follow

--- Assets/Scripts/WeaponFollow.cs
+++ Assets/Scripts/WeaponFollow.cs
@@ -2,10 +2,12 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponFollow : MonoBehaviour
 {
     public GameObject player;
+    public Image staminaFill;
     public Combat combatScript;
     public float radius = 1.0f;
     private Rigidbody2D playerRigidbody;
@@ -28,28 +30,35 @@ public class WeaponFollow : MonoBehaviour
         {new Vector2(-0.8f, -0.8f), 315f}, // Downwards Left
     };
 
+    float Remap(float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
     Vector2 _lastNormalizedVelocity = new Vector2(0f, 0f);
-    float _deltaTimeOnLastRotation = Time.time;
+    float _deltaTimeOnLastRotation = 0;
     public void FixedUpdate() {
+        float _currentAttackSpeed = combatScript.currentSwordAttackSpeed;
+        float _timeSinceLastAttack = Time.time - _deltaTimeOnLastRotation;
+        if (_timeSinceLastAttack > 0.35) {
+            combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed - 0.05f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
+            if (_timeSinceLastAttack > 1) combatScript.currentSwordAttackSpeed = combatScript.fastestAttackSpeed;
+        } else if (_timeSinceLastAttack > 0.1) {
+            combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed - 0.015f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
+        }
+
         Vector2 _velocityNormalized = new Vector2(playerRigidbody.velocity.x, playerRigidbody.velocity.y).normalized;
         _velocityNormalized = new Vector2(Mathf.Floor(_velocityNormalized.x * 10f) / 10f, Mathf.Floor(_velocityNormalized.y * 10f) / 10f);
 
         if (_lastNormalizedVelocity != _velocityNormalized) {
-            float _timeSinceLastAttack = Time.time - _deltaTimeOnLastRotation;
-            float _currentAttackSpeed = combatScript.currentSwordAttackSpeed;
-
-            if (_timeSinceLastAttack < 0.15) {
-                if (_timeSinceLastAttack <= 0.08) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed += 0.1f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
-                if (_timeSinceLastAttack > 0.08) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed += 0.05f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
-            } else {
-                if (_timeSinceLastAttack <= 1 && _timeSinceLastAttack > 0.4) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed -= 0.5f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
-                if (_timeSinceLastAttack <= 0.4) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed -= 0.2f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
-                if (_timeSinceLastAttack > 1) combatScript.currentSwordAttackSpeed = combatScript.fastestAttackSpeed;
+            if (_timeSinceLastAttack < 0.2) {
+                if (_timeSinceLastAttack <= 0.08) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed + 0.05f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
+                if (_timeSinceLastAttack > 0.08) combatScript.currentSwordAttackSpeed = Mathf.Clamp(_currentAttackSpeed + 0.025f, combatScript.fastestAttackSpeed, combatScript.slowestAttackSpeed);
             }
 
             transform.DORotate(new Vector3(0, 0, _velocityToRotation[_velocityNormalized]), combatScript.currentSwordAttackSpeed);
             _deltaTimeOnLastRotation = Time.time;
         }
+        staminaFill.fillAmount = Remap(combatScript.currentSwordAttackSpeed, combatScript.slowestAttackSpeed, combatScript.fastestAttackSpeed, 0, 1);
         _lastNormalizedVelocity = _velocityNormalized;
     }
 }
